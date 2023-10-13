18a12e275b4607519129182fce66a86ca8b12b0a
arberlisaj <arberlisaj1@gmail.com>
2023-10-13 16:37:29 +0200

    updates

--- 14_password-finder-game/src/App.tsx
+++ 14_password-finder-game/src/App.tsx
@@ -1,50 +1,95 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const patterns = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "m",
+];
 
 const App = () => {
-  const [enteredNumbers, setEnteredNumbers] = useState<number[]>([]);
-  const [password, setPassword] = useState("1111");
-  const inputRef = useRef<HTMLInputElement>(null);
+  const [enteredPassword, setEnteredPassword] = useState<string[]>([]);
+  const [password, setPassword] = useState("javascript");
 
-  useEffect(() => {
-    if (enteredNumbers.length === 4) {
-      if (enteredNumbers.join("") === password) {
-        alert("correct password");
-        setEnteredNumbers([]);
+  const handleChangePassword = () => {
+    const newPassword = enteredPassword.join("");
+    if (newPassword.length >= 6) {
+      setPassword(newPassword);
+      setEnteredPassword([]);
+    } else {
+      alert("Bad password. Min length is 6");
+    }
+  };
+  const handleDeletePassword = () => {
+    const newArray = [...enteredPassword];
+    newArray.pop();
+    setEnteredPassword(newArray);
+  };
+  const handleSubmit = () => {
+    if (enteredPassword.length >= 6) {
+      if (enteredPassword.join("") === password) {
+        alert("logged in");
+        setEnteredPassword([]);
       } else {
         alert("wrong password");
-        setEnteredNumbers([]);
       }
     }
-  }, [enteredNumbers]);
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (inputRef.current?.value.length == 4)
-      setPassword(inputRef.current!.value);
   };
   return (
     <main>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <input
-          ref={inputRef}
-          maxLength={4}
-          type="text"
-          required
-          placeholder="password"
-        />
-        <button>change password</button>
-      </form>
       <h1>Password: {password}</h1>
-      <h1>Entered Password {enteredNumbers.join("")}</h1>
-      {numbers.map((n) => (
-        <button
-          onClick={() => setEnteredNumbers((value) => [...value, n])}
-          key={n}
-        >
-          {n}
+      <h1 style={{ marginBottom: "5px" }}>Entered: {enteredPassword}</h1>
+      <div className="buttons">
+        {patterns.map((n) => (
+          <button
+            onClick={() => setEnteredPassword((value) => [...value, n])}
+            key={n}
+          >
+            {n}
+          </button>
+        ))}
+        <button className="delete" onClickCapture={handleDeletePassword}>
+          del
         </button>
-      ))}
+        <button className="span" onClick={handleChangePassword}>
+          change password
+        </button>
+      </div>
+      <button onClick={handleSubmit} className="login">
+        Log in
+      </button>
     </main>
   );
 };
